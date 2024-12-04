281ac681d122d6a58ea821e385002a05384df58f
Micro-Organism <coolrabbit1993@gmail.com>
2024-12-04 17:46:45 +0800

    feat: springboot 集成 apache nifi

---
+++ zero-apache-nifi/src/main/java/com/zero/apache/nifi/ZeroApacheNifiApplication.java
@@ -0,0 +1,13 @@
+package com.zero.apache.nifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroApacheNifiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroApacheNifiApplication.class, args);
+    }
+
+}
---
+++ zero-apache-nifi/src/test/java/com/zero/apache/nifi/ZeroApacheNifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.apache.nifi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApacheNifiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
